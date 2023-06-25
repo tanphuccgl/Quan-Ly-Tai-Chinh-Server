7de6b57ae264a6dddfdbb58a8437a7c9ce8ae343
Levi <phamnguyentanphucgl@gmail.com>
2023-06-25 14:06:32 +0700

    fix api them khoang thu

--- insertKhoanThu.php
+++ insertKhoanThu.php
@@ -1,16 +1,22 @@
 <?php
 require"dbConnect.php";
 
-	$tendangnhap =$_POST['TenDangNhap'];
-	$tenloaithu =$_POST['TenLoaiThu'];
-	$sotienthu =$_POST['SoTienThu'];
-	
-	$a=$_POST['NgayThu'];
+// $tendangnhap ="a";
+// 	$tenloaithu ="fas";
+// 	$sotienthu ="30";
+// 	$a="2019-03-28";
+	$tendangnhap =$_GET['TenDangNhap'];
+	$tenloaithu =$_GET['TenLoaiThu'];
+	$sotienthu =$_GET['SoTienThu'];
+	$a=$_GET['NgayThu'];
 	$ngay =strtr($a,'/','-');
 	$ngaythu = date("Y-m-d", strtotime($ngay));
 
+	
+	
 
-	$query="INSERT INTO `khoanthu` (`makhoanthu`,`tendangnhap`, `tenloaithu` ,`sotienthu`, `ngaythu`) VALUES (NULL,'".$tendangnhap."','".$tenloaithu."','".$sotienthu."','".$ngaythu."')";
+
+	$query="INSERT INTO `khoanthu` (`makhoanthu`,`tendangnhap`, `tenloaithu` ,`sotienthu`, `ngaythu`) VALUES (NULL,'$tendangnhap','$tenloaithu','$sotienthu','$ngaythu')";
 
 if (mysqli_query($connect,$query)) {
 			echo "true";
